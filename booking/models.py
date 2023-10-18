952fd56220a234c8c0afec51f20cf8f688aa9620
Graham White <greywhite994@gmail.com>
2023-10-18 13:20:47 +0000

    Created Reservation class in models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    date = models.DateField(null=False, blank=False)
+    time = models.TimeField(null=False, blank=False)
+    guest_number = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
