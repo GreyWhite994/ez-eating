86cf48a9f1ef790c929246d1edc4d97bbbade5f4
Graham White <greywhite994@gmail.com>
2023-10-21 11:48:03 +0000

    Linked database to template and rendered data

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Reservation
 
 # Create your views here.
 
 
 def get_booking_list(request):
-    return render(request, 'booking/booking_list.html')
+    reservations = Reservation.objects.all()
+    context = {
+        'reservations': reservations
+    }
+    return render(request, 'booking/booking_list.html', context)
