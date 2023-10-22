5d25e003bb9fd6a08fe01bdf28cec8d22838764b
Graham White <greywhite994@gmail.com>
2023-10-22 17:30:51 +0000

    Created form to allow user to submit data and create reservation object in database

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Reservation
 
 # Create your views here.
@@ -10,3 +10,15 @@ def get_booking_list(request):
         'reservations': reservations
     }
     return render(request, 'booking/booking_list.html', context)
+
+
+def create_reservation(request):
+    if request.method == 'POST':
+        name = request.POST.get('reservation_name')
+        date = request.POST.get('reservation_date')
+        time = request.POST.get('reservation_time')
+        guest_number = request.POST.get('reservation_guest_number')
+        Reservation.objects.create(
+            name=name, date=date, time=time, guest_number=guest_number)
+        return redirect('get_booking_list')
+    return render(request, 'booking/create_reservation.html')
