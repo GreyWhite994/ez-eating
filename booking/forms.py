77a30feba8d3341edab12ec05b23c8046e967d10
Graham White <greywhite994@gmail.com>
2023-11-20 13:44:10 +0000

    Adddition of comments to forms.py file

--- booking/forms.py
+++ booking/forms.py
@@ -3,6 +3,9 @@ from .models import Reservation, Review
 import datetime
 from django.core.exceptions import ValidationError
 
+
+"""time_choices and guest_number choices to prevent user from picking a disallowed time
+or going over the max amount of allowed persons per table"""
 time_choices = [(datetime.time(hour=x), '{:02d}:00'.format(x)) for x in range(11, 22)]
 guest_number_choices = (
     ("1", 1),
@@ -15,6 +18,13 @@ guest_number_choices = (
 
 
 class ReservationForm(forms.ModelForm):
+    """ ReservationForm class
+
+    Takes Reservation Model.
+    Includes a function to prevent users from choosing a date in the past when making a reservation.
+    Includes Meta class and widgets for date, time and guest_number.
+    Time and guest_number take constraints to limit their options
+    """
     def clean_date(self):
         date = self.cleaned_data['date']
         if date < datetime.date.today():
@@ -32,6 +42,13 @@ class ReservationForm(forms.ModelForm):
         }
         
 class ReviewForm(forms.ModelForm):
+    """ReviewForm class
+
+    Takes Review Model. 
+    Allows users to input name and their review of the restuarant.
+    Date of review is imported from datetime.
+
+    """
     class Meta:
         model = Review
         fields = ['name', 'review']
