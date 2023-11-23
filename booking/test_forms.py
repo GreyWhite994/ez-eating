001790710c75a27e737c45eb749fc8fe46154261
Graham White <greywhite994@gmail.com>
2023-11-23 14:23:28 +0000

    Addition of test to test_forms.py

--- booking/test_forms.py
+++ booking/test_forms.py
@@ -1,1 +1,9 @@
 from django.test import TestCase
+from .forms import ReservationForm, ReviewForm
+
+class TestReservationForm(TestCase):
+    def test_reservation_name_is_required(self):
+        form = ReservationForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
