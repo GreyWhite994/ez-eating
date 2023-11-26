e5b00479a8297460b1bb028e3da188560417bf5f
Graham White <greywhite994@gmail.com>
2023-11-26 19:46:04 +0000

    Revert "Addition of tests to test_views.py"
    This reverts commit c221366a785d704ba8265f3a861b319874538445.

--- booking/tests_views.py
+++ booking/tests_views.py
@@ -8,22 +8,3 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
         self.assertTemplateUsed(response, 'base.html')
-
-    def test_get_booking_list_page(self):
-        response = self.client.get('/booking')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'booking_list.html')
-        self.assertTemplateUsed(response, 'base.html')
-
-    def test_get_create_reservation_page(self):
-        response = self.client.get('/create')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'create_reservation.html')
-        self.assertTemplateUsed(response, 'base.html')
-
-    def test_get_edit_reservation_page(self):
-        reservation = Reservation.objects.create(name='Test Reservation Item')
-        response = self.client.get(f'/edit/{item.id}')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'edit_reservation.html')
-        self.assertTemplateUsed(response, 'base.html')
