00b93c96ab29e1d9f589bad500d8a1fe95e5a2d7
Graham White <greywhite994@gmail.com>
2023-11-26 14:56:09 +0000

    Addition of TestViews class to test_views.py

--- booking/tests_views.py
+++ booking/tests_views.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+from .models import Reservation, Review
+
+class TestViews(TestCase):
+
+    def test_get_home(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'base.html')
